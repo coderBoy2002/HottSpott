8a69a58b925500180720460d9877abc7043c5697
coderBoy2002 <phutecker@g.hmc.edu>
2024-03-24 19:13:51 -0700

    Started application

---
+++ HottSpott/HottSpott/HottSpottApp.swift
@@ -0,0 +1,17 @@
+//
+//  HottSpottApp.swift
+//  HottSpott
+//
+//  Created by Patrick Hutecker on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct HottSpottApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
