e86dab72f65b1e0a794e9262670875905a5a5c2c
Ethan Ravelo <ethan.ravelo02@gmail.com>
2024-03-24 21:04:20 -0700

    Integreated camera position in LocationsViewModel

--- HottSpott/HottSpott/ViewModels/LocationsViewModel.swift
+++ HottSpott/HottSpott/ViewModels/LocationsViewModel.swift
@@ -6,15 +6,32 @@
 //
 
 import Foundation
-
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current map location
+    @Published var mapLocation: Location
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    @Published var cameraPosition: MapCameraPosition = MapCameraPosition.region(MKCoordinateRegion())
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapLocation(location: locations.first!)
+    }
+    
+    private func updateMapLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: location.coordinates, span: mapSpan))
+        }
     }
     
 }
--- HottSpott/HottSpott/Views/LocationsView.swift
+++ HottSpott/HottSpott/Views/LocationsView.swift
@@ -13,12 +13,9 @@ struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
     
-    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.89, longitude: 12.49), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)))
-    
-    
     var body: some View {
         ZStack {
-            Map(position: $cameraPosition)
+            Map(position: $vm.cameraPosition)
                 .ignoresSafeArea()
         }
     }
