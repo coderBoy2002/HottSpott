2cf3be526198ea46787325cf741ef212f53472bd
coderBoy2002 <phutecker@g.hmc.edu>
2024-03-26 21:38:54 -0700

    Did the lava lamp effect

---
+++ HottSpott/HottSpott/Views/MorphingCircleTexture.swift
@@ -0,0 +1,18 @@
+//
+//  MorphingCircleTexture.swift
+//  HottSpott
+//
+//  Created by Patrick Hutecker on 3/26/24.
+//
+
+import SwiftUI
+
+struct MorphingCircleTexture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MorphingCircleTexture()
+}
