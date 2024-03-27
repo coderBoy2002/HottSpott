1e68694305e4bb535fc95043ded375462b9f65a2
coderBoy2002 <phutecker@g.hmc.edu>
2024-03-26 19:57:33 -0700

    fixed half of the animation

---
+++ HottSpott/HottSpott/Views/SwipeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeView.swift
+//  HottSpott
+//
+//  Created by Patrick Hutecker on 3/26/24.
+//
+
+import SwiftUI
+
+struct SwipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeView()
+}
