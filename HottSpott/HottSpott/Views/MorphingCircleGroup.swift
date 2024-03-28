1daf423df781cb8f2d9135ed069f86b9245ce674
coderBoy2002 <phutecker@g.hmc.edu>
2024-03-28 14:35:42 -0700

    started developing more structs for lava lamp animation

--- HottSpott/HottSpott/Views/MorphingCircleGroup.swift
+++ HottSpott/HottSpott/Views/MorphingCircleGroup.swift
@@ -1,18 +1,18 @@
 //
-//  LocationDetailView.swift
+//  MorphingCircleGroup.swift
 //  HottSpott
 //
-//  Created by Patrick Hutecker on 3/24/24.
+//  Created by Patrick Hutecker on 3/28/24.
 //
 
 import SwiftUI
 
-struct LocationDetailView: View {
+struct MorphingCircleGroup: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LocationDetailView()
+    MorphingCircleGroup()
 }
