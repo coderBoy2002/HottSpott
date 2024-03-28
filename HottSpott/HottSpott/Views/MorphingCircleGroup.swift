11435404e60f26d7ce8c2c6f6f68439d98594636
coderBoy2002 <phutecker@g.hmc.edu>
2024-03-28 14:36:00 -0700

    started doing structs for laval lamp

--- HottSpott/HottSpott/Views/MorphingCircleGroup.swift
+++ HottSpott/HottSpott/Views/MorphingCircleGroup.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct MorphingCircleGroup: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State var size: CGFloat
+    @State var numCircles: Int
+    @State var morphingRange: CGFloat
+    @State var points: Int
+    @State var duration: Double
+    @State var secting: Double
+    @State var color: Color
+    @State var imageOffsets: [CGPoint]
+    
+    init(size: CGFloat, numCircles: Int, morphingRange: CGFloat, points: Int, duration: Double, secting: Double, color: Color, imageOffsets: [CGPoint]) {
+        self.size = size
+        self.numCircles = numCircles
+        self.morphingRange = morphingRange
+        self.points = points
+        self.duration = duration
+        self.secting = secting
+        self.color = color
+        self.imageOffsets = imageOffsets
     }
-}
-
-#Preview {
-    MorphingCircleGroup()
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<numCircles) { index in
+                MorphingCircle(size, morphingRange: morphingRange, color: color, points: points,  duration: duration, secting: secting)
+                    .position(
+                        x: imageOffsets[index].x,
+                        y: imageOffsets[index].y
+                    )
+            }
+        }
+        .ignoresSafeArea()
+    }
 }
--- HottSpott/HottSpott/Views/MorphingCircleTexture.swift
+++ HottSpott/HottSpott/Views/MorphingCircleTexture.swift
@@ -8,48 +8,43 @@
 import SwiftUI
 
 struct MorphingCircleTexture: View {
-    
-    @State var size: CGFloat
-    @State var numCircles: Int
-    @State var morphingRange: CGFloat
-    @State var points: Int
-    @State var duration: Double
-    @State var secting: Double
-    @State var color: Color
-    @State private var imageOffsets: [CGPoint]
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
     
-    init(size: CGFloat, numCircles: Int, morphingRange: CGFloat, points: Int, duration: Double, secting: Double, color: Color) {
-        self.size = size
-        self.numCircles = numCircles
-        self.morphingRange = morphingRange
-        self.points = points
-        self.duration = duration
-        self.secting = secting
+    @State var color: Color
+    
+    init (color: Color) {
         self.color = color
-        self.imageOffsets = Array(repeating: CGPoint.zero, count: numCircles)
     }
     
     var body: some View {
-        ZStack {
-            ForEach(0..<numCircles) { index in
-                MorphingCircle(size, morphingRange: morphingRange, color: color, points: points,  duration: duration, secting: secting)
-                    .position(
-                        x: imageOffsets[index].x,
-                        y: imageOffsets[index].y
-                    )
-            }
-        }
-        .ignoresSafeArea()
-        .onAppear {
-            for index in 0..<numCircles {
-                imageOffsets[index] = CGPoint(x: .random(in: 0..<screenWidth), y: .random(in: 0..<screenHeight))
-            }
-        }
+        bottomLayer
     }
+    
 }
 
 #Preview {
-    MorphingCircleTexture(size: 100, numCircles: 70, morphingRange: 30, points: 10, duration: 5.0, secting: 2, color: Color.blue)
+    MorphingCircleTexture(color: Color.blue)
 }
+
+extension MorphingCircleTexture {
+    private var numCirclesBottom: Int {
+        2
+    }
+    private var imageOffsetsBottom: [CGPoint] {
+        var imageOffsets: [CGPoint] = Array(repeating: CGPoint.zero, count: numCirclesBottom)
+        for index in 0..<numCirclesBottom {
+            imageOffsets[index] = CGPoint(
+                x: .random(in: 0..<screenWidth),
+                y: .random(in: 0..<screenHeight)
+            )
+        }
+        return imageOffsets
+    }
+    private var bottomLayer: some View {
+
+        return MorphingCircleGroup(size: 500, numCircles: numCirclesBottom, morphingRange: 60, points: 8, duration: 5.0, secting: 2, color: color, imageOffsets: imageOffsetsBottom)
+    }
+}
+
+
